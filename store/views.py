927e936aa381570a9f60e6b1765b9938d035c4d7
Sneha Roy <roysneha9635@gmail.com>
2021-03-15 19:55:18 +0530

    First commit

---
+++ store/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models.product import Product
+def index(request):
+    prds = Product.get_all_products();
+    return render(request, 'index.html', {'products': prds})
+
+
+
+
+
+
+
